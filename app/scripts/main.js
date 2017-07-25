c2a15d73aaafaf60e32d1eb472cbbdb919561aca
Zakeria Ali <zak.fined@gmail.com>
2017-07-25 22:39:08 +0100

    Add the play functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,39 @@
 $(document).ready(function() {
   $('.player').on('mouseover', function() {
-    $('.player__controls').css({
-      "position": "relative",
-      "bottom": "100px",
-      "transition": "all ease .6s"
-    });
+    if(document.body.clientWidth > 1700){
+      $('.player__controls').css({
+        "position": "relative",
+        "bottom": "110px",
+        "transition": "all ease .6s"
+      });
+    }
   });
   $('.player').on('mouseleave', function() {
-    $('.player__controls').css({
-      "position": "relative",
-      "bottom": "65px",
-      "transition": "all ease .6s"
-    });
+    if(document.body.clientWidth > 1700){
+      $('.player__controls').css({
+        "position": "relative",
+        "bottom": "66px",
+        "transition": "all ease .6s"
+      });
+    }
+  });
+
+  var player = $('.player');
+  var animation = $('.viewer');
+  var progress = $('.progress');
+  var progressBar = $('.progress__filled');
+  var playButton = $('.toggle');
+  var range = $('.slider');
+  var skip = $('.skip');
+
+  playButton.on('click', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    console.log(animation);
+    if (!animation.paused) {
+      animation.trigger('play');
+    } else {
+      animation.trigger('pause');
+    }
   });
 });
