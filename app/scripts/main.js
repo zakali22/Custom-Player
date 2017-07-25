acd0e4c3e54487e10524e71f12243b9baf4bbba6
Zakeria Ali <zak.fined@gmail.com>
2017-07-25 19:44:21 +0100

    Add the hovering functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('.player').on('mouseover', function() {
+    $('.player__controls').css({
+      "position": "relative",
+      "bottom": "100px",
+      "transition": "all ease .6s"
+    });
+  });
+  $('.player').on('mouseleave', function() {
+    $('.player__controls').css({
+      "position": "relative",
+      "bottom": "65px",
+      "transition": "all ease .6s"
+    });
+  });
+});
